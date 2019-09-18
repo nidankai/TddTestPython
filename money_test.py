204ee1609d3b923e18360d468dd8ac02225e38fe
nidankai <b4068@oic.jp>
2019-09-18 22:51:52 +0900

    3章　明白な実装終了時点

--- money_test.py
+++ money_test.py
@@ -9,6 +9,9 @@ class MyTestCase(unittest.TestCase):
         product = five.times(3)
         self.assertEqual(15, product.amount)
 
+    def test_equality(self):
+        self.assertTrue(dollar.Dollar(5).equals(dollar.Dollar(5)))
+        self.assertFalse(dollar.Dollar(5).equals(dollar.Dollar(6)))
 
 if __name__ == '__main__':
     unittest.main()
