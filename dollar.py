2287e7e7c3659c98b07793ca87ab949d8dc08425
nidankai <b4068@oic.jp>
2019-09-19 22:15:32 +0900

    4章　意図を語るテスト終了時点

--- dollar.py
+++ dollar.py
@@ -4,6 +4,11 @@ class Dollar:
     def __init__(self, amount):
         self.amount = amount
 
+    def __eq__(self, other):
+        if not isinstance(other, Dollar):
+            return NotImplemented
+        return self.amount == other.amount
+
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
--- money_test.py
+++ money_test.py
@@ -4,10 +4,8 @@ import dollar
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         five = dollar.Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(dollar.Dollar(10), five.times(2))
+        self.assertEqual(dollar.Dollar(15), five.times(3))
 
     def test_equality(self):
         self.assertTrue(dollar.Dollar(5).equals(dollar.Dollar(5)))
